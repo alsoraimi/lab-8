5497bf3cdbef0f93e12be6ceb1bad6d41d53b11c
alsoraimi <alsoraimi46@gmail.com>
2017-04-18 15:16:28 -0400

    added loop and ask user if continue

--- Lab 8/Program.cs
+++ Lab 8/Program.cs
@@ -9,11 +9,20 @@ namespace Lab_8
     class Program
     {
         static void Main(string[] args)
-        {
+        { bool loop = true;                 //added for loop
+            do
+            { 
             Console.WriteLine("Welcome to Batting Average Calculator!");
             Console.Write("Enter number of times at bat: ");
             int[] TimesBatting = new int[GetValidInteger(1, 25)];
             GetBattingResults(TimesBatting);
+
+            if (!ContinueLoop())
+            {
+                loop = false;
+            }
+
+        } while (loop);
         }
 
         public static int GetValidInteger(int Min, int Max)
@@ -26,7 +35,7 @@ namespace Lab_8
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Error: Input not a number."); //Prompts error if input is not a number
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
                     Console.Write("Try again: ");
                     continue;
                 }
@@ -34,7 +43,7 @@ namespace Lab_8
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Error: Input not between {Min} and {Max}."); //Prompts error if input is not between Min and Max
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
                     Console.Write("Try again: ");
                     continue;
                 }
@@ -61,7 +70,31 @@ namespace Lab_8
             Console.WriteLine($"Batting Average: {BattingAverage}");
             Console.WriteLine($"Slugging Percentage: {SluggingPercent}");
         }
+        public static bool ContinueLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to try again, ? (Y/N)"); // add user input if they want to try again
+                string input = Console.ReadLine().ToUpper();
+                if (input == "Y")
+                {
+                    Console.WriteLine();
+                    return true;
+                }
+
+                else if (input == "N")
+                {
+                    Console.WriteLine("thanks for trying!");
+                    return false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;            // changes color of text for error
+                    Console.WriteLine("ERROR, ENTER Y OR N");
+                    Console.ForegroundColor = ConsoleColor.Black;         //changes color of text back to original
+
+                }
+            }
+        }
     }
 }
-//SP = SUM/Number of Atbats
-//BA = number of Atbats not zero / number of Atbats
